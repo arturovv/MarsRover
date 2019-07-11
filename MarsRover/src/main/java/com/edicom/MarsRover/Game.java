84bf6eee6ee006f8bbad72ca03c000d577926722
avaron <avaron@ED1939.edicom.es>
2019-07-11 12:57:59 +0200

    [green] goForward

---
+++ MarsRover/src/main/java/com/edicom/MarsRover/Game.java
@@ -0,0 +1,20 @@
+package com.edicom.MarsRover;
+
+public class Game {
+	
+
+	public static Rover goForward(Rover rover) {
+		
+		switch(rover.getDirection()) {
+			case 'N' : rover.setX(rover.getX()-1); break;
+			case 'S' : rover.setX(rover.getX()+1); break;
+			case 'W' : rover.setY(rover.getY()-1); break;
+			case 'E' : rover.setY(rover.getY()+1); break;
+			default : ;
+		}
+		
+		return rover;
+
+	}
+
+}
---
+++ MarsRover/src/main/java/com/edicom/MarsRover/Rover.java
@@ -0,0 +1,57 @@
+package com.edicom.MarsRover;
+
+public class Rover {
+	
+	private int x, y;
+	private char direction;
+	
+	
+	public Rover(int x, int y, char direction) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.direction = direction;
+	}
+
+
+	public int getX() {
+		return x;
+	}
+
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+
+	public int getY() {
+		return y;
+	}
+
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+
+	public char getDirection() {
+		return direction;
+	}
+
+
+	public void setDirection(char direction) {
+		this.direction = direction;
+	}
+	
+	public int[] getPos() {
+		
+		int [] returnInt = { this.getX(), this.getY() };
+		
+		return returnInt;
+		
+	}
+	
+	
+	
+
+}
