1eb096cb52893c88ceb22c950863c62996cd75c0
avaron <avaron@ED1939.edicom.es>
2019-07-11 12:30:45 +0200

    [red] testForward

--- MarsRover/src/test/java/com/edicom/MarsRover/RoverTest.java
+++ MarsRover/src/test/java/com/edicom/MarsRover/RoverTest.java
@@ -1,7 +1,33 @@
 package com.edicom.MarsRover;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+
+@RunWith(DataProviderRunner.class)
 public class RoverTest {
 	
-	@Test
+	@DataProvider
+	public static Object[][] dataRoverProviderForForward() {
 
+		return new Object[][] { 
+			{ new Rover(0, 0, "W"), 0, 0 }, 
+			{ new Rover(1, 2, "E"), 1, 3 }, 
+			{ new Rover(2, 0, "S"), 3, 0 } 
+		};
+	}
+	
+	@Test
+	@UseDataProvider("dataRoverProviderForForward")
+	public void testForward(Rover rover, int expectedPosX, int expectedPosY) {
+		
+		rover.goForward();
+		int [] expected = { expectedPosX, expectedPosY };
+		Assert.assertEquals(expected, rover.getPos());
+		
+	}
 }
